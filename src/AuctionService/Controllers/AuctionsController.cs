0db03823fa33c7178eb2a235910eeacbd250447d
lchen9 <lchen9@nd.edu>
2023-11-01 23:22:55 -0700

    SearchService v1 & Http messaging

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -4,6 +4,7 @@ using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,14 +21,15 @@ public class AuctionsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
     {
-        var auctions = await _context.Auctions
-            .Include(x => x.Item)
-            .OrderBy(x => x.Item.EventName)
-            .ToListAsync();
-        
-        return _mapper.Map<List<AuctionDto>>(auctions);
+        var query = _context.Auctions.OrderBy(x => x.Item.EventName).AsQueryable();
+
+        if (!string.IsNullOrEmpty(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+        return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
     }
 
     [HttpGet("{id}")]
---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿namespace SearchService.Data;
+
+using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.Services;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(x => x.EventName, KeyType.Text)
+            .Key(x => x.EventCategory, KeyType.Text)
+            .Key(x => x.Guest, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        using var scope = app.Services.CreateScope();
+
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine(items.Count + " returned from the auction service");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
+    }
+}
